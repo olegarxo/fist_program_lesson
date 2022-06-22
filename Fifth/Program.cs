8e71a2e04408ce609fb9780632738198aa9ff81a
oleg <olegarxo15@gmail.com>
2022-06-22 16:32:27 +0300

    Сделали 5 задание

---
+++ Fifth/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Привет Маша");
+}
+
+else {
+    Console.Write("Привет");
+    Console.WriteLine(username);
+}
